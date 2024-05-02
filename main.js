9f0803beed10465c92b82fcf19af362fc36b498e
shanmugharajk <shanmugharaj.k@outlook.com>
2024-05-02 21:43:46 +0530

    feat: add ui

--- main.js
+++ main.js
@@ -1,16 +1,32 @@
-try {
-  const response = await fetch("http://localhost:3000/api/todos");
-  const data = await response.json();
-  console.log(data);
-} catch (error) {
-  console.log(error);
-}
+document.getElementById("btn-todos").addEventListener("click", async () => {
+  try {
 
+    document.getElementById("result").innerHTML = "";
 
-try {
-  const response = await fetch("http://localhost:3000/api/bikes");
-  const data = await response.json();
-  console.log(data);
-} catch (error) {
-  console.log(error);
-}
+    const response = await fetch("http://localhost:3000/api/todos");
+    const data = await response.json();
+    console.log(data);
+
+    document.getElementById("result").innerHTML = JSON.stringify(data, null, 2);
+  } catch (error) {
+    console.log(error);
+
+    document.getElementById("result").innerHTML = error;
+  }
+});
+
+document.getElementById("btn-bikes").addEventListener("click", async () => {
+  try {
+    document.getElementById("result").innerHTML = "";
+
+    const response = await fetch("http://localhost:3000/api/bikes");
+    const data = await response.json();
+    console.log(data);
+
+    document.getElementById("result").innerHTML = JSON.stringify(data, null, 2);
+  } catch (error) {
+    console.log(error);
+
+    document.getElementById("result").innerHTML = error;
+  }
+});
